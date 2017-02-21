1b0bee0c0989f96439d3c2f78173277bda93cb0c
Ash Laidlaw and Benjamin T. Seaver <benjamin@>
2017-02-21 13:31:27 -0800

    Add due date to category view and post /tasks/ route

--- app/app.php
+++ app/app.php
@@ -29,7 +29,8 @@
     $app->post("/tasks", function() use ($app) {
         $description = $_POST['description'];
         $category_id = $_POST['category_id'];
-        $task = new Task($description, $id = null, $category_id);
+        $due_date = $_POST['due_date'];
+        $task = new Task($description, $id = null, $category_id, $due_date);
         $task->save();
         $category = Category::find($category_id);
         return $app['twig']->render('category.html.twig', array('category' => $category, 'tasks' => $category->getTasks()));
@@ -38,12 +39,6 @@
     $app->get("/categories/{id}", function($id) use ($app) {
         $category = Category::find($id);
         return $app['twig']->render('category.html.twig', array('category' => $category, 'tasks' => $category->getTasks()));
-    });
-
-    $app->post("/tasks", function() use ($app) {
-        $task = new Task($_POST['description']);
-        $task->save();
-        return $app['twig']->render('create_task.html.twig', array('newtask' => $task));
     });
 
     $app->post("/delete_tasks", function() use ($app) {
