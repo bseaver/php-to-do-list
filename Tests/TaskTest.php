a3cf1fde3fc4daf1b06c677af822abd0029ddebb
Ash Laidlaw and Benjamin T. Seaver <benjamin@>
2017-02-21 13:40:29 -0800

    Update testGetTasks_With_Due_Date test to sort by date

--- Tests/TaskTest.php
+++ Tests/TaskTest.php
@@ -195,12 +195,12 @@
             $test_category_id = $test_category->getId();
 
             $description = "Email client";
-            $due_date = '2017-02-22';
+            $due_date = '2017-03-22';
             $test_task = new Task($description, $id, $test_category_id, $due_date);
             $test_task->save();
 
             $description2 = "Meet with boss";
-            $due_date2 = '2017-03-01';
+            $due_date2 = '2017-02-01';
             $test_task2 = new Task($description2, $id, $test_category_id, $due_date2);
             $test_task2->save();
 
@@ -208,7 +208,7 @@
             $result = $test_category->getTasks();
 
             //Assert
-            $this->assertEquals([$test_task, $test_task2], $result);
+            $this->assertEquals([$test_task2, $test_task], $result);
         }
     }
 ?>
