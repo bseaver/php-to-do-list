4c865149f847d24bfd4b25145518a01c3b725934
Ash Laidlaw and Benjamin T. Seaver <benjamin@>
2017-02-21 14:45:55 -0800

    Add method that deletes categories and tassks within the category

--- Tests/CategoryTest.php
+++ Tests/CategoryTest.php
@@ -130,7 +130,7 @@
             //Assert
             $this->assertEquals("Home stuff", $test_category->getName());
         }
-        
+
         function testDelete()
         {
             //Arrange
@@ -149,6 +149,27 @@
 
             //Assert
             $this->assertEquals([$test_category2], Category::getAll());
-        }    }
+        }
 
+        function testDeleteCategoryTasks()
+        {
+            //Arrange
+            $name = "Work stuff";
+            $id = null;
+            $test_category = new Category($name, $id);
+            $test_category->save();
+
+            $description = "Build website";
+            $category_id = $test_category->getId();
+            $test_task = new Task($description, $id, $category_id);
+            $test_task->save();
+
+
+            //Act
+            $test_category->delete();
+
+            //Assert
+            $this->assertEquals([], Task::getAll());
+        }
+    }
 ?>
